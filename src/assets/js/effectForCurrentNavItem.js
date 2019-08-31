5aa51ddc2a10be88af8da731e380d71bab8d41ed
Noat <nmtoan251998@gmail.com>
2019-08-31 15:49:19 +0700

    v0.1.0: re-markup

--- src/assets/js/effectForCurrentNavItem.js
+++ src/assets/js/effectForCurrentNavItem.js
@@ -1,24 +1,22 @@
 document.addEventListener("DOMContentLoaded", function() {
     // current navbar
     const aboutEle = document.getElementById('about');
-    const socialEle = document.getElementById('socials');
+    const contactEle = document.getElementById('contact');
     const skillEle = document.getElementById('skills');
     const workEle = document.getElementById('works');
     const eduEle = document.getElementById('edu');
-    const contactEle = document.getElementById('contact');
     
     // current position
     const aboutSectionYOffset = aboutEle.offsetTop;
-    const socialSectionYOffset = socialEle.offsetTop;
+    const contactSectionYOffset = contactEle.offsetTop;    
     const skillSectionYOffset = skillEle.offsetTop;
     const workSectionYOffset = workEle.offsetTop;
     const eduSectionYOffset = eduEle.offsetTop;
-    const contactSectionYOffset = contactEle.offsetTop;    
 
     const navItems = document.querySelectorAll('.nav__item');
 
     window.addEventListener('scroll', function() {
-        if (this.pageYOffset >= aboutSectionYOffset-100 && this.pageYOffset < socialSectionYOffset-180)  {
+        if (this.pageYOffset >= aboutSectionYOffset-100 && this.pageYOffset < contactSectionYOffset-180)  {
             navItems.forEach(item => {
                 if(item.classList.contains('current')) {
                     item.classList.remove('current');
@@ -26,14 +24,14 @@ document.addEventListener("DOMContentLoaded", function() {
             });
 
             document.querySelector('.nav__about').classList.add('current');
-        } else if (this.pageYOffset >= socialSectionYOffset-180 && this.pageYOffset < skillSectionYOffset-50) {
+        } else if (this.pageYOffset >= contactSectionYOffset-180 && this.pageYOffset < skillSectionYOffset-50) {
             navItems.forEach(item => {
                 if(item.classList.contains('current')) {
                     item.classList.remove('current');
                 }
             });
 
-            document.querySelector('.nav__socials').classList.add('current');
+            document.querySelector('.nav__contact').classList.add('current');
         } else if (this.pageYOffset >= skillSectionYOffset-50 && this.pageYOffset < workSectionYOffset) {
             navItems.forEach(item => {
                 if(item.classList.contains('current')) {
@@ -42,7 +40,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
 
             document.querySelector('.nav__skills').classList.add('current');
-        } else if (this.pageYOffset >= workSectionYOffset && this.pageYOffset < eduSectionYOffset) {
+        } else if (this.pageYOffset >= workSectionYOffset-20 && this.pageYOffset < eduSectionYOffset) {
             navItems.forEach(item => {
                 if(item.classList.contains('current')) {
                     item.classList.remove('current');
@@ -50,7 +48,7 @@ document.addEventListener("DOMContentLoaded", function() {
             });
 
             document.querySelector('.nav__works').classList.add('current');
-        } else if (this.pageYOffset >= eduSectionYOffset && this.pageYOffset < contactSectionYOffset-200) {
+        } else if (this.pageYOffset >= eduSectionYOffset-180) {
             navItems.forEach(item => {
                 if(item.classList.contains('current')) {
                     item.classList.remove('current');
@@ -58,14 +56,6 @@ document.addEventListener("DOMContentLoaded", function() {
             });
 
             document.querySelector('.nav__edu').classList.add('current');
-        } else if (this.pageYOffset >= contactSectionYOffset-200) {
-            navItems.forEach(item => {
-                if(item.classList.contains('current')) {
-                    item.classList.remove('current');
-                }
-            });
-
-            document.querySelector('.nav__contact').classList.add('current');
         } else {
             navItems.forEach(item => {
                 if(item.classList.contains('current')) {
